d8867d77ae163594ca8ac7c6c5eab9b0f346c697
guilherme-ribeiro27 <guilherme.ribeiro062002@gmail.com>
2024-06-06 22:17:17 -0300

    Adjust on routing

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,13 @@ import DarkMode from './components/DarkMode.vue'
 </script>
 
 <template>
-  <main class='w-full min-h-screen flex flex-col gap-y-64 items-center bg-background dark:bg-dark-background'>
-    <DarkMode />
+  <!-- <main class='w-full min-h-screen flex flex-col gap-y-56 items-center bg-background dark:bg-dark-background'> -->
+    <!-- <DarkMode /> -->
     <!-- <MyDiv  /> -->
-    <RouterView />
+    <!-- <RouterView /> -->
 
-  </main>
+  <!-- </main> -->
+  <RouterView />
 </template>
 
 
