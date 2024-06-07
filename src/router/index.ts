80337acc2aea1f1258237922a30d69be57d544a8
guilherme-ribeiro27 <guilherme.ribeiro062002@gmail.com>
2024-06-06 22:00:38 -0300

    Adjust on routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,33 +2,81 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
+import DashboardView from '@/views/DashboardView.vue'
+import AuthLayout from '@/components/AuthLayout.vue'
+import NoAuthLayout from '@/components/NoAuthLayout.vue'
 import Guard from '../services/middleware'
+import Cookie from 'js-cookie';
+import Auth from '../services/auth'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      component: NoAuthLayout,
+      children:[
+        {
+          path: '/',
+          name: 'home',
+          component: HomeView
+        },
+        {
+          path: '/login',
+          name: 'login',
+          component : LoginView
+        },
+        {
+          path: '/register',
+          name: 'register',
+          component: RegisterView
+        },
+      ]
     },
     {
-      path: '/login',
-      name: 'Login',
-      // route level code-splitting
-      component : LoginView
-    },
-    {
-      path: '/register',
-      name: 'Register',
-      component: RegisterView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      beforeEnter: Guard.auth,
-      component: () => import('../views/AboutView.vue')
+      path:'/',
+      component: AuthLayout,
+      children:[
+        {
+          path: '/dashboard',
+          name: 'dashboard',
+          component: DashboardView,
+        }
+      ]
     }
   ]
 })
 
+async function auth(to:any, from:any){
+  const token = Cookie.get('auth_token');
+  console.log(token);
+  if(!token){
+    router.push({
+      name: 'login'
+    })
+    return;
+  }
+  
+  //refresh token
+  try{
+    const result = await Auth.refresh(token);
+    if(result.status == 200){
+      router.push({
+        name: to.name
+      });
+      return;
+    }
+    else{
+      Cookie.remove('auth_token');
+      router.push({
+        name: 'login'
+      }); 
+    }
+  }catch(err){
+    //remove from cookie
+    Cookie.remove('auth_token');
+    router.push({
+      name: 'login'
+    });
+  }
+}
 export default router
