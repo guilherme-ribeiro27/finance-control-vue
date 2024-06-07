eb191ba5ac8e7119f7b0c6ca66ed592fa1e5c594
guilherme-ribeiro27 <guilherme.ribeiro062002@gmail.com>
2024-06-06 22:17:37 -0300

    Including ToastContainer

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import './assets/index.css'
+import 'vue3-toastify/dist/index.css';
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Vue3Toasity, { type ToastContainerOptions } from 'vue3-toastify';
 
 const app = createApp(App)
 
 app.use(router)
-
+app.use(Vue3Toasity,{
+    autoClose: 3000,
+} as ToastContainerOptions)
 app.mount('#app')
