c5a5a22edd2cbc4774b4a36ccc37c30589e7a2b6
guilherme-ribeiro27 <guilherme.ribeiro062002@gmail.com>
2024-06-09 22:27:36 -0300

    new layout components for auth and no auth pages

---
+++ src/components/AuthLayout.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import Sidebar from './SideBar/SideBar.vue';
+import DarkMode from './DarkMode.vue';
+
+
+</script>
+<template>     
+    <div class='relative flex bg-surface dark:bg-dark-surface min-h-screen'>
+        <Sidebar />
+        <div class="flex-1 min-h-screen">
+            <!-- header -->
+            <div class="header w-full h-[5%] bg-surface dark:bg-dark-surface">
+                <DarkMode />
+            </div>
+            <!-- Page Content -->
+            <div class="rounded-s-xl h-[95%]
+                bg-background dark:bg-dark-background
+                py-8 px-12 dark:text-white
+                "
+                >Page Content
+            </div>
+        </div>
+        <RouterView />
+    </div>
+    
+</template>
+
+<style lang="">
+    
+</style>
---
+++ src/components/NoAuthLayout.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import DarkMode from './DarkMode.vue'
+</script>
+<template>
+    <main class='w-full min-h-screen flex flex-col gap-y-56 items-center bg-background dark:bg-dark-background'>
+        <DarkMode />
+        <!-- <MyDiv  /> -->
+        <RouterView />
+    
+      </main>
+</template>
+
+<style lang="">
+    
+</style>
