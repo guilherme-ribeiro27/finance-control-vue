3af87f97a4eb2b6cb0f59bf19679093d28384bf4
guilherme-ribeiro27 <guilherme.ribeiro062002@gmail.com>
2024-06-12 14:44:05 -0300

    Structure changes

--- src/components/SideBar/SideBarNav.vue
+++ src/components/SideBar/SideBarNav.vue
@@ -1,70 +1,31 @@
 <script setup lang="ts">
-import type { DefineComponent } from 'vue';
 import SideBarNavItem from './SideBarNavItem.vue'
 import DashboardIcon from './icons/DashboardIcon.vue';
 import CardsIcon from './icons/CardsIcon.vue';
+import ExpensesIcon from './icons/ExpensesIcon.vue';
+import InvestimentsIcon from './icons/InvestimentsIcon.vue';   
+import SettingsIcon from './icons/SettingsIcon.vue';
+
 interface NavItem{
     title: string;
-    icon: any;
+    icon: Object;
 }
-console.log(typeof DashboardIcon)
 const navItems: NavItem[] = [
     {title: 'Dashboard', icon: DashboardIcon},
     {title: 'Cards', icon: CardsIcon},
-    {title: 'Expenses', icon: DashboardIcon},
-    {title: 'Investments', icon: DashboardIcon},
-    {title: 'Settings', icon: DashboardIcon},
+    {title: 'Expenses', icon: ExpensesIcon},
+    {title: 'Investments', icon: InvestimentsIcon},
+    {title: 'Settings', icon: SettingsIcon},
 ]
 </script>
 
 <template>
-    <nav>
+    <nav class="px-4 pr-6 mt-14 flex flex-col ">
         <SideBarNavItem 
             v-for="item in navItems"
             :key="item.title"
             :title="item.title"
             :icon="item.icon"
-        >
-            <!-- <template #icon>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-                </svg>
-            </template> -->
-        </SideBarNavItem>
-        <!-- <SideBarNavItem title="Dashboard">
-            <template #icon>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-                </svg>
-            </template>
-        </SideBarNavItem> -->
-        <SideBarNavItem title="Cards">
-            <template #icon>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z" />
-                </svg>
-            </template>
-        </SideBarNavItem>
-        <SideBarNavItem title="Expenses">
-            <template #icon>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
-                </svg>
-            </template>
-        </SideBarNavItem>
-        <SideBarNavItem title="Investments">
-            <template #icon>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z" />
-                </svg> 
-            </template>
-        </SideBarNavItem>
-        <SideBarNavItem title="Settings">
-            <template #icon>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" />
-                </svg>
-            </template>
-        </SideBarNavItem>
-   </nav>
+        />
+    </nav>
 </template>
--- src/components/SideBar/SideBarNavItem.vue
+++ src/components/SideBar/SideBarNavItem.vue
@@ -1,18 +1,17 @@
 <script setup lang="ts">
-import { defineProps } from 'vue';
 interface Props {
     title: string;
-    icon: any;
+    icon: Object;
 }
-const props = defineProps<Props>()
-console.log(props.icon)
+defineProps<Props>()
+
 </script>
 
-
+<!-- rounded-lg
+border-2 -->
 <template>
-    <a href="" class='flex flex-row items-center'>
-        <!-- <slot name="icon"></slot> -->
-         {{ icon }}
+    <a href="" class='flex flex-row items-center text-secondary dark:text-dark-secondary gap-2 py-3 px-2 rounded-lg hover:text-primary hover:dark:text-primary hover:border hover:dark:bg-dark-hightlight'>
+        <component :is="icon"></component>
         <span>{{title}}</span>
     </a>
 </template>
