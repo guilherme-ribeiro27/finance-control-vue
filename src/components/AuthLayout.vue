120d71aaf6f2c3de48655999ee8536119f606261
guilherme-ribeiro27 <guilherme.ribeiro062002@gmail.com>
2024-06-12 14:43:44 -0300

    Changes on css

--- src/components/AuthLayout.vue
+++ src/components/AuthLayout.vue
@@ -9,7 +9,12 @@ import DarkMode from './DarkMode.vue';
         <Sidebar />
         <div class="flex-1 min-h-screen">
             <!-- header -->
-            <div class="header w-full h-[5%] bg-surface dark:bg-dark-surface">
+            <div class="flex flex-row header w-full h-[5%] bg-surface dark:bg-dark-surface">
+                <button class="h-12 text-primary">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h8m-8 6h16" />
+                    </svg>
+                </button>
                 <DarkMode />
             </div>
             <!-- Page Content -->
